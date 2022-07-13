2f52fd3b93780066d245c7a243a87c3d95908551
temo <chxaidzetemo@gmail.com>
2022-07-14 02:00:37 +0400

    222

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,10 +2,16 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-        while (power != 1) {
-            if (power % 2 != 0)
-                System.out.println(power);
-            power = power / 2;
+        if (power<0){
+            System.out.println("too much power");
+        }else {
+            int i =1;
+            int k=0;
+            while (k<=power) {
+                System.out.println(i);
+                i*=2;
+                k++;
+            }
         }
     }
 }
